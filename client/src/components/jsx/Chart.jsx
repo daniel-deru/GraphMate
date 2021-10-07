dfd9f4eeb8d31fe39d1b2edd444fda6bd51ae820
daniel <danielderu@gmail.com>
2021-10-07 07:27:50 +0200

    busy adding values for pie chart to see which options the users will have.

--- client/src/components/jsx/Chart.jsx
+++ client/src/components/jsx/Chart.jsx
@@ -7,15 +7,13 @@ const Chart = () => {
             <Plot
                 data={[
                 {
-                    x: [1, 2, 3],
-                    y: [2, 6, 3],
-                    type: 'scatter',
-                    mode: 'lines+markers',
-                    marker: {color: 'red'},
-                },
-                {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                    values: [19, 26, 55],
+                    labels: ['Residential', 'Non-Residential', 'Utility'],
+                    type: 'pie',
+                    hole: 0.1
+                }
                 ]}
-                layout={ {width: 720, height: 440, title: 'A Fancy Plot'} }
+                layout={ {width: 620, height: 340, title: 'A Fancy Plot'} }
            />
         </div>
     )
--- client/src/components/jsx/Pie.jsx
+++ client/src/components/jsx/Pie.jsx
@@ -1,9 +1,40 @@
 import "../css/Pie.css"
+import "../../index.css"
+import { useState } from "react"
 
 const Pie = () => {
+    const [graphData, setGraphData] = useState({type: "pie"})
     return (
         <div>
-            <h1>This is the pie</h1>
+            <div className="form-field-container">
+                <label htmlFor="title">Enter Your Title</label>
+                <input type="text" name="title" placeholder="title" id="" />
+            </div>
+            <div className="form-field-container">
+                <label htmlFor="theme">Choose Your Theme</label>
+                <select name="colors" name="theme" id="">
+                    <option value="" disabled selected>Select Color Theme</option>
+                    <option value="Default">Default</option>
+                    <option value="Rainbow">Rainbow</option>
+                    <option value="Blue">Blue</option>
+                    <option value="Red">Red</option>
+                    <option value="Green">Green</option>
+                    <option value="Yellow">Yellow</option>
+                </select>
+            </div>
+            <div  className="form-field-container">
+                <label htmlFor="labels">Enter Labels</label>
+                <textarea name="labels" id="pie-labels" placeholder="Example: Label1, Label2, Label3" cols="30" rows="10"></textarea>
+            </div>
+            <div  className="form-field-container">
+                <label htmlFor="values">Enter Values</label>
+                <textarea name="values" id="pie-values" placeholder="Example: 2, 3, 4" cols="30" rows="10"></textarea>
+            </div>
+            {/* this is specific to pie charts */}
+            <div className="form-field-container">
+                <label htmlFor="hole">Donut Size</label>
+                <input type="number" placeholder="Choose a number between 0 and 99"/>
+            </div>
         </div>
     )
 }
